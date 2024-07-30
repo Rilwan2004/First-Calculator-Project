3dc3f091c7afe97e78dfbf58a9ebd64bb4650ace
Rilwan Yusuf <rilwanyusuf2004@gmail.com>
2024-07-30 23:40:55 +0100

    added functionality for basic operators

--- script.js
+++ script.js
@@ -2,9 +2,51 @@ var displayNumber = $(".display-input");
 var currentNumber = '';
 var previousNumber = '';
 var operator = '';
+var displayContent = ''
 
 $(".digit").on("click", function () {
     var value = $(this).val()
     currentNumber += value
-    displayNumber.val(currentNumber)
+    displayContent += value
+    displayNumber.val(displayContent)
 })
+
+$(".operator").on("click", function () {
+    var value = $(this).val()
+    if (value === "=") {
+        previousNumber = calculate(previousNumber, operator, currentNumber)
+        displayNumber.val(previousNumber)
+        currentNumber = '';
+        operator = '';
+        displayContent= previousNumber
+    } else {
+        if (currentNumber !== '') {
+            if (previousNumber === '') {
+                previousNumber = currentNumber;
+            } else {
+                previousNumber = calculate(previousNumber, operator, currentNumber);
+            }
+            operator = value;
+            currentNumber = '';
+            displayContent = previousNumber + operator
+            displayNumber.val(displayContent)
+        }
+    }
+})
+function calculate(x, operator, y) {
+    x = parseFloat(x);
+    y = parseFloat(y);
+
+    switch (operator) {
+        case '+':
+            return (x + y).toString();
+        case '-':
+            return (x - y).toString();
+        case '*':
+            return (x * y).toString();
+        case '/':
+            return (x / y).toString();
+        default:
+            return y;
+    }
+}
